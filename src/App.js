16d3a04f0909a6de32b0493f0a23298065c01ed5
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-08 22:07:58 +0100

    added conditional renderign for login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import React, { useState } from 'react';
 import Home from './components/home/';
+import Login from './components/login';
 // import logo from './logo.svg';
 
 function App() {
+  const [user, setUser] = useState(null);
+  // const [loggedIn, setIsLoggedIn] = useState(false);
+
+  if (user === null) {
+    return <Login />
+  }
+
   return (
     <div className="App">
       <Home />
