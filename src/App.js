c83c96e505487bc207677ef43ad1c579e7040f0e
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-08 18:58:53 +0100

    Created a home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-// import logo from './logo.svg';
 import './App.css';
+import Home from './components/home/';
+// import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <h1>LiteBank App</h1>
+      <Home />
     </div>
   );
 }
