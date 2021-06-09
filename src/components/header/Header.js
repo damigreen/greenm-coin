7aa6efb16c95033300227d77fd21032c4805e382
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-09 15:36:43 +0100

    Added layout for header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
 
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="header-wrap flex-col">
+      <div className='user-avatar'></div>
+      <h3 className='user-name'>Damilola Faseun</h3>
+      <h6 className="user-mail">damilola.faseun@gmail.com</h6>
     </div>
   )
 }
