d2a63e67fae318aa216c7f718f9c06025610a60e
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-09 10:37:03 +0100

    created router between signup and login

--- src/App.js
+++ src/App.js
@@ -2,20 +2,55 @@ import './App.css';
 import React, { useState } from 'react';
 import Home from './components/home/';
 import Login from './components/login';
+import Signup from './components/signup';
+import {
+  BrowserRouter as Router,
+  // HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+
 // import logo from './logo.svg';
 
 function App() {
+  const [page, setPage] = useState('home')
   const [user, setUser] = useState(null);
+  // const [user, setUser] = useState('dami');
   // const [loggedIn, setIsLoggedIn] = useState(false);
 
   if (user === null) {
-    return <Login />
+    
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Login
+              show={page === 'login'}
+            />
+          </Route>
+
+          <Route path='/signup'>
+            <Signup
+              show={page === 'signup'}
+            />
+          </Route>
+
+        </Switch>
+      </Router>
+    )
   }
 
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Home
+          show={page === 'home'}
+        />
+      </div>
+    </Router>
   );
 }
 
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,45 +2,59 @@ import './Login.css';
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 const Login = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   return (
-    <div className="login-wrap flex-col bg-login">
-      <div className="login-row flex-col">
-        <div className='login-intro'>
-          <h1>Let's Get Started</h1>
-          <p>Login to your account to start sending and receiving money at your convenience</p>
+    <Router>
+      <Switch>
+        <div className="login-wrap flex-col bg-login">
+          <div className="login-row flex-col">
+            <div className='login-intro'>
+              <h1>Let's Get Started</h1>
+              <p>Login to your account to start sending and receiving money at your convenience</p>
+            </div>
+
+            <Form className='form-wrap'>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button style={{width: '100%'}} variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+
+
+            <div style={{marginTop: "3rem"}}>
+              <Link to="/signup" style={{color: '#c4c4c4',fontSize: "1.02732rem"}}>Dont Have an account? <span style={{color: '#17e2be'}}>Signup!</span></Link>
+            </div>
+          </div>
         </div>
 
-        <Form className='form-wrap'>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+      </Switch>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-
-
-        <div>
-          <p style={{color: 'blue'}}>Create new account!</p>
-        </div>
-      </div>
-    </div>
+    </Router>
   )
 }
 
