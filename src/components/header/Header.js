5e3f291b35e0736ca003c43d4a11813c38ddba6a
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-13 16:26:45 +0100

    Logout logic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Header.css';
+import logoutIcon from '../../assets/images/box-arrow-left.svg';
 
-function Header({ user }) {
-  const welcomeMessage = (message) => {
+function Header({ user, handleLogout }) {
+  const welcomeMessage = () => {
     return (
       <div>
         <h3 className='user-name'></h3>
@@ -14,8 +15,12 @@ function Header({ user }) {
   return (
     <div className="header-wrap flex-col">
       <div className='user-avatar'></div>
-      <h4 className='user-name'>{user.name} is <span style={{color: '#27B8AC', fontStyle: 'italics' }}>loggedIn</span></h4>
+      <h4 className='user-name'>{user.name} is <span style={{color: '#27B8AC', fontStyle: 'italics' }}>loggedin</span></h4>
       <h6 className="user-mail">{`${user.email}`}</h6>
+      <a className="logout flex-row" onClick={handleLogout}>
+        <img style={{marginBottom: '0.561rem', marginRight: '0.431rem', width: 25, height: 25}} alt='logout icon' src={logoutIcon} />
+        <h5 style={{color: '#f45d48'}}>Logout</h5>
+      </a>
     </div>
   )
 }
