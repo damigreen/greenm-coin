f0f0bc77a4911388df2e33356657e3723bda15d5
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-13 15:31:06 +0100

    Handle login logic

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -8,53 +8,68 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import useField from '../../hook/';
+import loginServices from '../../services/login';
 
 
 
-const Login = () => {
+const Login = ({ setUser }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const email = useField('email').form;
+  const password = useField('password').form;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  
+    console.log(`Loginin in ----`);
+
+    const loginObj = {
+      email: email.value,
+      password: password.value,
+    }
+
+    loginServices.login(loginObj).then(result => {
+      setUser(result);
+    })
+  }
+  
+
 
   return (
-    // <Router>
-    //   <Switch>
-        <div className="login-wrap flex-col bg-login">
-          <div className="login-row flex-col">
-            <div className='login-intro'>
-              <h1>Let's Get Started</h1>
-              <p>Login to your account to start sending and receiving money at your convenience</p>
-            </div>
-
-            <Form className='form-wrap'>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button style={{width: '100%'}} variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-
-
-            <div style={{marginTop: "3rem"}}>
-              <Link to="/signup" style={{color: '#c4c4c4',fontSize: "1.02732rem"}}>Dont Have an account? <span style={{color: '#17e2be'}}>Signup!</span></Link>
-            </div>
-          </div>
+    <div className="login-wrap flex-col bg-login">
+      <div className="login-row flex-col">
+        <div className='login-intro'>
+          <h1>Let's Get Started</h1>
+          <p>Login to your account to start sending and receiving money at your convenience</p>
         </div>
 
-    //   </Switch>
+        <Form onSubmit={handleLogin} className='form-wrap'>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control { ...email } placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-    // </Router>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control { ...password } placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button style={{width: '100%'}} variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+
+
+        <div style={{marginTop: "3rem"}}>
+          <Link to="/signup" style={{color: '#c4c4c4',fontSize: "1.02732rem"}}>Dont Have an account? <span style={{color: '#17e2be'}}>Signup!</span></Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
